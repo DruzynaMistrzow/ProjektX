54431a3a369b7a35e38a2644b3adc4dc1db26d84
Teteran <kacper3d3@gmail.com>
2018-04-02 18:07:59 +0200

    Dodanie Reduxa

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {Provider} from 'react-redux'
+import {StyleSheet, Text, View} from 'react-native';
+import store from "./src/store/index";
+import {StackNavigator} from 'react-navigation'
+import MainScreen from './src/screens/main/view/MainScreen'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+
+        const MainNavigator = StackNavigator({
+            Main:{screen: MainScreen}
+        })
+        return (
+            <Provider store={store}>
+                <MainNavigator/>
+            </Provider>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
