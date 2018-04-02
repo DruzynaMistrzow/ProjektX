815c8236bae1d35181f95986a8e73a46816aa5f0
Teteran <kacper3d3@gmail.com>
2018-04-02 18:14:32 +0200

    Eslint

--- Mobile/src/reducers/index.js
+++ Mobile/src/reducers/index.js
@@ -1,5 +1,5 @@
-import {combineReducers} from 'redux'
-import exampleReducer from './exampleReducer'
+import { combineReducers } from 'redux';
+import exampleReducer from './exampleReducer';
 
 export default combineReducers({
     exampleReducer
