3ba32f7170b625f4259e48048ac2573020af115b
Teteran <kacper3d3@gmail.com>
2018-04-04 23:07:45 +0200

    Added some components

---
+++ Mobile/src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <View style={styles.header}>
+                <Text style={styles.text}>
+                    {this.props.headerTitle}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f3a017',
+        height: 45
+    },
+    text: {
+        textAlign: 'center',
+        color: '#bec6ff',
+        fontWeight: 'bold',
+        fontSize: 18,
+    }
+});
