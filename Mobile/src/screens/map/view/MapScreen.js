58e30b9009804eaec5957c71e367458f1215db9a
Teteran <kacper3d3@gmail.com>
2018-04-11 19:47:42 +0200

    Upgrade MapScreen

--- Mobile/src/screens/map/view/MapScreen.js
+++ Mobile/src/screens/map/view/MapScreen.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import { MapView } from 'expo';
+import { MapView, Location } from 'expo';
 import { Screen } from '../../../components';
 
 
 export default class MapScreen extends Component {
+    state = {
+        mapRegion: null,
+        locationResult: null,
+    };
+
+    componentDidMount() {
+        this._getLocationAsync();
+    }
+
+    _getLocationAsync = async () => {
+        const location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location) });
+
+        this.setState({
+            mapRegion: {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.01
+            }
+        });
+    };
 
     render() {
         return (
             <Screen title="Map">
+                {this.state.mapRegion &&
                 <MapView
                     style={{ flex: 1 }}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
+                    region={this.state.mapRegion}
+                    showsUserLocation
+                    provider="google"
+                />}
             </Screen>
+
         );
     }
-
 }
