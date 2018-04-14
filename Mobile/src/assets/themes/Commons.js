2a24726902357fa42c495e876a273da69f7d9b52
Teteran <kacper3d3@gmail.com>
2018-04-14 16:06:32 +0200

    Imput text style

--- Mobile/src/assets/themes/Commons.js
+++ Mobile/src/assets/themes/Commons.js
@@ -4,6 +4,13 @@ const commons = {
         alignItems: 'center',
         justifyContent: 'center'
     },
+    inputText: {
+        fontSize: 20,
+        borderWidth: 1,
+        borderColor: '#d4d3cf',
+        paddingHorizontal: 10,
+        marginVertical: 10
+    }
 };
 
 export default commons;
